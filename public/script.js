7687389fede679df7f8ac7d69beb1dcf1687d0e9
Jan-Yves Ruzicka <jan@1klb.com>
2014-04-07 15:06:07 +1200

    Updated icon type

---
+++ public/script.js
@@ -0,0 +1,33 @@
+function actOnClass(cls,func) {
+  Array.prototype.forEach.call(document.querySelectorAll("."+cls), func);
+}
+
+function hideInactive() {
+  var hide = function(el){ el.style.display = "none" };
+  actOnClass("deferred", hide);
+  actOnClass("waiting-on", hide);
+  actOnClass("hanging", hide);
+  actOnClass("hideable", hide);
+
+  var button = document.querySelector(".toggle-hanging");
+  button.classList.remove("typcn-folder-delete");
+  button.classList.add("typcn-folder-add");
+  button.onclick = showInactive;
+}
+
+function showInactive() {
+  var show = function(el){  el.style.display = "" };
+  actOnClass("deferred", show);
+  actOnClass("waiting-on", show);
+  actOnClass("hanging", show);
+  actOnClass("hideable", show);
+
+  var button = document.querySelector(".toggle-hanging");
+  button.classList.remove("typcn-folder-add");
+  button.classList.add("typcn-folder-delete");
+  button.onclick = hideInactive;
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelector(".toggle-hanging").onclick = hideInactive;
+})
